d48c8053ec923ae9b7a52bf05882f125dcf9fbc7
Andre Chanthbouala <chanthbouala@gmail.com>
2022-11-07 11:18:18 +0100

    Corrected: pydantic class of transmitted data

--- fastAPI_backend.py
+++ fastAPI_backend.py
@@ -12,8 +12,6 @@ from typing import Optional
 with open('model_pipeline.pickle', 'rb') as handle:
     pipeline = pickle.load(handle)
 
-#df_data = pd.read_csv('df_application_test.zip', compression='zip', header=0, sep=',', quotechar='"')
-
 def replace_none(test_dict):
     # checking for dictionary and replacing if None
     if isinstance(test_dict, dict):
@@ -23,56 +21,45 @@ def replace_none(test_dict):
     return test_dict
             
 class Frontend_data(BaseModel):
-    AMT_CREDIT: float
-    AMT_ANNUITY: float
+    ACTIVE_AMT_CREDIT_SUM_DEBT_MAX: Optional[float]
+    AMT_ANNUITY: Optional[float]
+    AMT_CREDIT: Optional[float]
     AMT_GOODS_PRICE: Optional[float]
+    ANNUITY_INCOME_PERC: Optional[float]
+    APPROVED_CNT_PAYMENT_MEAN: Optional[float]
+    CLOSED_DAYS_CREDIT_MAX: Optional[float]
     CODE_GENDER: Optional[bool]
     DAYS_BIRTH: Optional[int]
-    NAME_FAMILY_STATUS: Optional[str]
-    NAME_EDUCATION_TYPE: Optional[str]
-    ORGANIZATION_TYPE: Optional[str]
     DAYS_EMPLOYED: Optional[int]
-    ACTIVE_AMT_CREDIT_SUM_DEBT_MAX: Optional[float]
-    PAYMENT_RATE: Optional[float]
-    ANNUITY_INCOME_PERC: Optional[float]
     DAYS_ID_PUBLISH: Optional[int]
-    REGION_POPULATION_RELATIVE: Optional[float]
-    FLAG_OWN_CAR: Optional[bool]
-    OWN_CAR_AGE: Optional[float]
     FLAG_DOCUMENT_3: Optional[bool]
-    CLOSED_DAYS_CREDIT_MAX: Optional[float]
+    FLAG_OWN_CAR: Optional[bool]
     INSTAL_AMT_PAYMENT_SUM: Optional[float]
-    APPROVED_CNT_PAYMENT_MEAN: Optional[float]
-    PREV_CNT_PAYMENT_MEAN: Optional[float]
-    PREV_APP_CREDIT_PERC_MIN: Optional[float]
-    INSTAL_DPD_MEAN: Optional[float]
     INSTAL_DAYS_ENTRY_PAYMENT_MAX: Optional[float]
+    INSTAL_DPD_MEAN: Optional[float]
+    NAME_EDUCATION_TYPE: Optional[str]
+    NAME_FAMILY_STATUS: Optional[str]
+    ORGANIZATION_TYPE: Optional[str]
+    OWN_CAR_AGE: Optional[float]
+    PAYMENT_RATE: Optional[float]
     POS_MONTHS_BALANCE_SIZE: Optional[float]
+    PREV_APP_CREDIT_PERC_MIN: Optional[float]
+    PREV_CNT_PAYMENT_MEAN: Optional[float]
+    REGION_POPULATION_RELATIVE: Optional[float]
+    AMT_INCOME_TOTAL: Optional[float]
 
-class ID(BaseModel):
-    SK_ID_CURR: int
-
-#app = FastAPI(default_response_class=ORJSONResponse)
 app = FastAPI()
 
 @app.get('/')
 def index():
     return {'message': "You've entered the API backend"}
-'''
-@app.get('/get_ids')
-async def get_ids():
-    return {'IDs': df_data["SK_ID_CURR"].tolist()}
 
-@app.post('/get_data', response_class=ORJSONResponse)
-async def get_data(data: ID):
-    return {'Data': [df_data.loc[df_data["SK_ID_CURR"] == data.dict()["SK_ID_CURR"]].to_dict(orient='records')]}
-'''
 @app.post('/predict')
 async def predict_risk(data: Frontend_data):
     dict_data = replace_none(data.dict())
     df = pd.DataFrame([dict_data])
     for col in df.columns:
-        if df[col].dtype == 'object':
+        if df[col].dtype == 'object' or df[col].dtype == 'str':
             df[col] = df[col].astype('category')
     pred = pipeline.predict_proba(df)[:, 1].tolist()
     result = {"Probability": pred}
